701f83c2d0a58c44c5ae6d58dab04aaea4150fb2
bruce <bruce.monnerville@hotmail.fr>
2024-08-11 20:51:55 +0200

    design a propos

--- src/components/Valeurs/index.jsx
+++ src/components/Valeurs/index.jsx
@@ -1,13 +1,14 @@
 import "../../styles/Valeurs.css"
+import arrow from "../../assets/Vector.png"
 
 function Valeurs(){
     return (
         <section className="valeurs">
 
-            <div className="fiabilite"><p></p></div>
-            <div className="respect"><p></p></div>
-            <div className="service"><p></p></div>
-            <div className="securite"><p></p></div>
+            <div className="bar"><p className="bar__text">Fiabilité</p><img src={arrow} className="bar__icon" /></div>
+            <div className="bar"><p className="bar__text">Respect</p><img src={arrow} className="bar__icon" /></div>
+            <div className="bar"><p className="bar__text">Service</p><img src={arrow} className="bar__icon" /></div>
+            <div className="bar"><p className="bar__text">Sécurité</p><img src={arrow} className="bar__icon" /> </div>
 
         </section>
     )
